6cfb9770a16581329b2ae2aa9c2b8aa2a3eff446
Siva Raja Surya E <sivarajasurya16@gmail.com>
2023-09-04 09:07:01 +0530

    04 09 23 9am

--- src/app/rooms/room.ts
+++ src/app/rooms/room.ts
@@ -1,11 +1,12 @@
 export interface Room {
-  availableRooms?: number;
-  bookedRooms?: number;
-  totalRooms?: number;
+  availableRooms: number;
+  bookedRooms: number;
+  totalRooms: number;
 }
 
 
 export interface RoomsList {
+  roomNumber: number;
     roomTypes: string;
     amenities : string;
     price: number;
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -20,6 +20,7 @@ export class RoomsComponent implements OnInit {
 
   roomList : RoomsList[] = [
     {
+      roomNumber: 1,
     roomTypes : 'Deluxe Room',
     amenities: 'Air Conditioner , Free Wi-Fi , TV, Bathroom',
     price: 500,
@@ -28,10 +29,20 @@ export class RoomsComponent implements OnInit {
     checkoutTime: new Date('12-Nov-2023'),
   },
   {
+    roomNumber: 2,
     roomTypes : 'Deluxe Room',
     amenities: 'Air Conditioner , Free Wi-Fi ,  Bathroom',
     price: 1000,
-    photos: 'https://pixabay.com/photos/interior-design-modern-style-home-4467768/',
+    photos: 'https://img.freepik.com/free-photo/armchair-green-living-room-with-copy-space_43614-910.jpg?w=1380&t=st=1693795654~exp=1693796254~hmac=ac149824d2d7c8eb7c01fd245ab3824cbb10fd73c445f991b17cbd625ffb51ed',
+    checkinTime: new Date('11-Nov-2023'),
+    checkoutTime: new Date('12-Nov-2023'),
+  },
+  {
+    roomNumber: 1,
+    roomTypes : 'Deluxe Room',
+    amenities: 'Air Conditioner , Free Wi-Fi ,  Bathroom',
+    price: 1500,
+    photos: 'https://img.freepik.com/free-vector/artist-studio-night-cartoon-illustration_107791-5974.jpg?w=1380&t=st=1693795688~exp=1693796288~hmac=c48ffa7dcbb28719f230fda8ed71e97a41d8b3928db6636501e0344b8e94ce20',
     checkinTime: new Date('11-Nov-2023'),
     checkoutTime: new Date('12-Nov-2023'),
   },
